d3293598160c9096d9ba75f4b8dd2f0b2a1c8e67
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-09-23 17:27:55 +0200

    le fameux script Curly fait son retour croustillant

---
+++ curly.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+#css="http://osp.club:9999/p/medor-css/export/txt"
+html="http://osp.club:9999/p/medor-html/export/txt"
+
+#curl -o "static/css/styles.less" "${css}"
+curl -o "subsribe/templates/home.html" "${html}"
