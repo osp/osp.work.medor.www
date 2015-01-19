b48c86a2607192a46b84493353d3efa01d16ff5b
Alexandre Leray <alexandre@stdin.fr>
2015-01-19 15:17:47 +0100

    Événéments depuis le calendrier

--- subscribe/templatetags/subscribe_tags.py
+++ subscribe/templatetags/subscribe_tags.py
@@ -14,3 +14,46 @@ def get_subscriber_count():
 @register.assignment_tag
 def get_capital_amount():
     return Cooperation.objects.exclude(status=2).aggregate(total=Sum('share_number'))['total'] * 20
+
+
+@register.assignment_tag
+def get_next_events():
+    import datetime
+    import pytz
+    import requests
+    from icalendar import Calendar
+
+
+    request = requests.get('http://dav.medor.coop/medor/medor-evenements.ics/', auth=('medor', 'felix'))
+    request.encoding = 'UTF-8'
+
+    gcal = Calendar.from_ical(request.text)
+
+    events = [c for c in gcal.walk() if c.name == 'VEVENT']
+
+
+    def to_datetime(dt):
+        if isinstance(dt, datetime.date):
+            return datetime.datetime.combine(dt, datetime.time(0, 0)).replace(tzinfo=pytz.timezone('Europe/Brussels'))
+        else:
+            return dt
+
+
+    def compare(item1, item2):
+        item1 = to_datetime(item1)
+        item2 = to_datetime(item2)
+
+        if item1 == item2:
+            return 0
+        elif item1 < item2:
+            return -1
+        else:
+            return 1
+
+    events.sort(key=lambda r: r.get('dtstart').dt, cmp=compare)
+
+    now = datetime.datetime.now()
+    now = now.replace(tzinfo=pytz.timezone('Europe/Brussels'))
+    events = [e for e in events if to_datetime(e.get('dtstart').dt) > now]
+
+    return events[:3]
