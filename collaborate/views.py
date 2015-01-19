f16989d00e7021a350a0610b0ed442dbd89bc2b2
Alexandre Leray <alexandre@stdin.fr>
2015-01-19 10:39:32 +0100

    le formulaire de collaboration scindé en 2 dans la vue

--- collaborate/views.py
+++ collaborate/views.py
@@ -7,6 +7,13 @@ class ArticleProposalView(FormView):
     form_class = ArticleProposalForm
     success_url = '/thanks/'
 
+    def get_context_data(self, **kwargs):
+        context = super(ArticleProposalView, self).get_context_data(**kwargs)
+        fields = list(context['form'])
+        context['part1'] = fields[:5]
+        context['part2'] = fields[5:]
+        return context
+
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
