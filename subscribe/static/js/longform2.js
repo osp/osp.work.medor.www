3949efebae798a41ea364a9bde4082ef861273c6
Alexandre Leray <alexandre@stdin.fr>
2015-02-25 10:37:54 +0100

    More changes to the new home page

--- subscribe/static/js/longform2.js
+++ subscribe/static/js/longform2.js
@@ -9,12 +9,12 @@ function remy(target) {
 $(function() {
     remy($("main"));
 
-    $("#menu").tocify({
-        "selectors": "h2",
-        "hashGenerator": "pretty",
-        "highlightOffset": 10,
-        "extendPage": false,
-    });
+    //$("#menu").tocify({
+        //"selectors": "h2[data-shortname]",
+        //"hashGenerator": "pretty",
+        //"highlightOffset": 10,
+        //"extendPage": false,
+    //});
     
     //if (Modernizr.details) {
         //console.log('support ok');
@@ -67,31 +67,74 @@ $(function() {
 
     new ScrollScene({triggerElement: "#magazine", duration: 1200, offset: -100, loglevel: 3})
         .setTween(detailIn)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
 
     new ScrollScene({triggerElement: "#collaborative-experience", duration: 1200, offset: -300})
         .setTween(detailOut)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
 
     new ScrollScene({triggerElement: "#collaborative-experience", duration: 1200, offset: -100})
         .setTween(coopIn)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
 
     new ScrollScene({triggerElement: "#accounting", duration: 1200, offset: -300})
         .setTween(coopOut)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
 
     new ScrollScene({triggerElement: "#accounting", duration: 1200, offset: -100})
         .setTween(budgetIn)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
 
     new ScrollScene({triggerElement: "#status", duration: 1200, offset: -300})
         .setTween(budgetOut)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
+        //.addIndicators();
+    
+
+        
+    $( window ).scroll( function( e ) {
+        // get all the header on top of the viewport
+        var headers = $('#intro, #participate, #status');
+        current = headers.map( function( e ) {
+            if ( ( $( this ).offset().top - $( window ).scrollTop() ) < 0 ) {
+                return this;
+            }
+        });
+        // get only the latest header on the viewport
+        current = $( current ).eq( current.length - 1 );
+
+        console.log(current);
+
+        //if ( current && current.length ) {
+            //// get all li tag that contains href of # ( all the parents )
+            //list = $( 'li:has(a[href="#' + current.attr( 'id' ) + '"])' );
+
+            //if ( prev !== undefined ) {
+                //prev.removeClass( 'active' );
+            //}
+
+            //list.addClass( 'active' );
+            //prev = list;
+        //}
+    });
+
+    //$('#menu a').each(function() {
+        //var that = this;
+        //var href = $(this).attr('href');
+        //new ScrollScene({triggerElement: $(href)})
+            //.addTo(controller)
+            //.on("enter leave", function (e) {
+                //console.log('ok');
+                //if (e.type == "enter") {
+                    //$(that).addClass('active');
+                //} else {
+                    //$(that).removeClass('active');
+                //}
+            //});
+    //});
 });
