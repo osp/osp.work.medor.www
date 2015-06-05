0ebdfb7dc0fadfd0b6e56ccc7a07e0f67695921c
Alexandre Leray <alexandre@stdin.fr>
2015-06-05 02:21:05 +0200

    Simplification du code formulaire

--- collaborate/views.py
+++ collaborate/views.py
@@ -9,76 +9,53 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class ArticleProposalThanksView(TemplateView):
     template_name = "collaborate/article_proposal_thanks.html"
 
 
-class ArticleProposalView(FormView):
+class ArticleProposalBaseView(FormView):
+    success_url = reverse_lazy('article-proposal-thanks')
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleProposalBaseView, self).get_context_data(**kwargs)
+        fields = list(context['form'])
+        context['part1'] = fields[:4]
+        context['part2'] = fields[4:5]
+        context['part3'] = fields[5:]
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        obj = form.instance
+        site = get_current_site(self.request)
+        admin_url = u"https://%s%s" % (site, urlresolvers.reverse('admin:collaborate_articleproposal_change', args=(obj.id,)))
+
+        subject = "Nouvelle demande de collaboration"
+        message = render_to_string('collaborate/article_proposal_alert.txt', {'obj': obj, 'admin_url': admin_url})
+        sender = "jaiuntrucpourvous@medor.coop"
+        recipients = ["jaiuntrucpourvous@medor.coop"]
+        send_mail(subject, message, sender, recipients)
+
+        subject = "Demande de collaboration"
+        message = render_to_string('collaborate/article_proposal_confirmation.txt', {'obj': obj})
+        sender = "jaiuntrucpourvous@medor.coop"
+        recipients = [obj.email]
+        send_mail(subject, message, sender, recipients)
+
+        return super(ArticleProposalBaseView, self).form_valid(form)
+
+
+class ArticleProposalView(ArticleProposalBaseView):
     template_name = 'collaborate/article_proposal.html'
     form_class = ArticleProposalForm
-    success_url = reverse_lazy('article-proposal-thanks')
-
-    def get_context_data(self, **kwargs):
-        context = super(ArticleProposalView, self).get_context_data(**kwargs)
-        fields = list(context['form'])
-        context['part1'] = fields[:4]
-        context['part2'] = fields[4:5]
-        context['part3'] = fields[5:]
-        return context
-
-    def form_valid(self, form):
-        form.save()
-        obj = form.instance
-        admin_url = urlresolvers.reverse('admin:collaborate_articleproposal_change', args=(obj.id,))
-
-        subject = "Nouvelle demande de collaboration"
-        message = render_to_string('collaborate/article_proposal_alert.txt', {'obj': obj, 'admin_url': admin_url})
-        sender = "jaiuntrucpourvous@medor.coop"
-        recipients = ["jaiuntrucpourvous@medor.coop"]
-        send_mail(subject, message, sender, recipients)
-
-        subject = "Demande de collaboration"
-        message = render_to_string('collaborate/article_proposal_confirmation.txt', {'obj': obj})
-        sender = "jaiuntrucpourvous@medor.coop"
-        recipients = [obj.email]
-        send_mail(subject, message, sender, recipients)
-
-        return super(ArticleProposalView, self).form_valid(form)
 
 
-class ArticleProposalSimpleView(FormView):
+class ArticleProposalSimpleView(ArticleProposalBaseView):
     template_name = 'collaborate/article_proposal_simple.html'
     form_class = ArticleProposalSimpleForm
-    success_url = reverse_lazy('article-proposal-thanks')
-
-    def get_context_data(self, **kwargs):
-        context = super(ArticleProposalSimpleView, self).get_context_data(**kwargs)
-        fields = list(context['form'])
-        context['part1'] = fields[:4]
-        context['part2'] = fields[4:5]
-        context['part3'] = fields[5:]
-        return context
-
-    def form_valid(self, form):
-        form.save()
-        obj = form.instance
-        admin_url = urlresolvers.reverse('admin:collaborate_articleproposal_change', args=(obj.id,))
-
-        subject = "Nouvelle demande de collaboration"
-        message = render_to_string('collaborate/article_proposal_alert.txt', {'obj': obj, 'admin_url': admin_url})
-        sender = "jaiuntrucpourvous@medor.coop"
-        recipients = ["jaiuntrucpourvous@medor.coop"]
-        send_mail(subject, message, sender, recipients)
-
-        subject = "Demande de collaboration"
-        message = render_to_string('collaborate/article_proposal_confirmation.txt', {'obj': obj})
-        sender = "jaiuntrucpourvous@medor.coop"
-        recipients = [obj.email]
-        send_mail(subject, message, sender, recipients)
-
-        return super(ArticleProposalSimpleView, self).form_valid(form)
 
 
 def can_read_proposals(user):
