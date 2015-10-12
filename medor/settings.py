48ad6d838f03f7d2f0cf8a8436d58475a6a5ba44
Alexandre Leray <alexandre@stdin.fr>
2015-10-12 17:11:21 +0200

    Added class "intro-medor" to the CKeditor whitelist

--- medor/settings.py
+++ medor/settings.py
@@ -236,7 +236,7 @@ CKEDITOR_CONFIGS = {
         'format_tags': 'p;h2;h3',
         'entities': False,
         'image2_captionedClass': '',
-        'extraAllowedContent': 'abbr figure figcaption aside; div p(chapeau); ol(footnotes)',
+        'extraAllowedContent': 'abbr figure figcaption aside; div p(chapeau,intro-medor); ol(footnotes)',
         'image2_prefillDimensions': False,
         'admin_url': '/admin/',
         'toolbar_Custom': [
