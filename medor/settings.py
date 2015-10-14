2aa81b2cbe3dadd5663ca536b550d7db8c56fb3f
Alexandre Leray <alexandre@stdin.fr>
2015-10-14 11:32:34 +0200

    H1 in the CKEditor whitelist

--- medor/settings.py
+++ medor/settings.py
@@ -236,7 +236,7 @@ CKEDITOR_CONFIGS = {
         'format_tags': 'p;h2;h3',
         'entities': False,
         'image2_captionedClass': '',
-        'extraAllowedContent': 'abbr figure figcaption aside; div p(chapeau,intro-medor,question); ol(footnotes)',
+        'extraAllowedContent': 'h1 abbr figure figcaption aside; div p(chapeau,intro-medor,question); ol(footnotes)',
         'image2_prefillDimensions': False,
         'admin_url': '/admin/',
         'toolbar_Custom': [
