c1f15f95896d692a50446a450d2f25b0ce2d4a68
marion <marion.chancerel@gmail.com>
2015-11-16 12:29:07 +0100

    Un bien bel effet sur le menu feed

---
+++ medor/static/medor/js/header.js
@@ -0,0 +1,16 @@
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            menu = document.querySelector(".menu");
+        if (distanceY > shrinkOn) {
+            classie.add(menu,"smaller");
+        } else {
+            if (classie.has(menu,"smaller")) {
+                classie.remove(menu,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
