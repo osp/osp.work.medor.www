2a4d0968e6af5ac24175724d9b2a88b56cd7c2e2
Alexandre Leray <alexandre@stdin.fr>
2015-12-05 11:14:08 +0100

    Fix formulaire simple

--- collaborate/forms.py
+++ collaborate/forms.py
@@ -168,7 +168,7 @@ class ArticleProposalSimpleForm(forms.ModelForm):
         body = []
 
         for f in fields:
-            data = cleaned_data.get(f)
+            data = cleaned_data.get(f, '')
 
             field = self.fields[f]
 
