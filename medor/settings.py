c5ec412bab348483b8814a8236bcd703a74f89a4
Alex Leray <alexandre@stdin.fr>
2016-02-16 17:50:54 +0100

    Span class auteur autorisé

--- medor/settings.py
+++ medor/settings.py
@@ -253,7 +253,7 @@ CKEDITOR_CONFIGS = {
         'format_normal': {'element': 'p', 'name' : 'Texte normal'},
         'entities': False,
         'image2_captionedClass': '',
-        'extraAllowedContent': 'h1 abbr figure figcaption footer small dl dt dd; div[class]; aside[id](exergue,making-of,encadre); p(chapeau,intro-medor,question,auteur,fin,debut); ol(footnotes); img[data-*]; video[controls,src]; span(lettrine,print-only); iframe[src,width,height,frameborder,webkitallowfullscreen,mozallowfullscreen,allowfullscreen]',
+        'extraAllowedContent': 'h1 abbr figure figcaption footer small dl dt dd; div[class]; aside[id](exergue,making-of,encadre); p(chapeau,intro-medor,question,auteur,fin,debut); ol(footnotes); img[data-*]; video[controls,src]; span(lettrine,print-only,auteur); iframe[src,width,height,frameborder,webkitallowfullscreen,mozallowfullscreen,allowfullscreen]',
         'image2_prefillDimensions': False,
         'admin_url': '/admin/',
         'contentsCss':'https://medor.coop/static/CACHE/css/f3b96be794f8.css',
