bc41c9adeb528a051eedbab25cf3fe23a531764b
Alex Leray <alexandre@stdin.fr>
2016-06-20 16:18:10 +0200

    Retail outlets as csv

--- buy/views.py
+++ buy/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from buy.models import RetailOutlet
+
+import unicodecsv
+from django.http import HttpResponse
+
+
+
+def retail_outlet_as_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="retail-outlets.csv"'
+
+    writer = unicodecsv.writer(response, encoding='utf-8')
+    writer.writerow([
+        "name",
+        "description",
+        "latitude",
+        "longitude",
+    ])
+
+    for r in RetailOutlet.objects.all():
+        writer.writerow([
+            r.name,
+            u"%s, %s %s, %s" % (r.address, r.zip_code, r.city, r.country),
+            u"%s" % r.latitude,
+            u"%s" % r.longitude
+        ])
+
+    return response
