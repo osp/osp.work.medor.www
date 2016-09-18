2f025ad478ea40b59edd5828e963b395ce4517f6
Alex Leray <alexandre@stdin.fr>
2016-09-19 01:32:53 +0200

    Removed old subscribe url

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-from subscribe.views import CooperationWizardView, SubscriptionWizardView, subscribers_as_csv, cooperators_as_csv, OrderWizardView
+from subscribe.views import CooperationWizardView, subscribers_as_csv, cooperators_as_csv, OrderWizardView
 
 
 urlpatterns = patterns('',
     url(r'^cooperation/$', CooperationWizardView.as_view(), name='cooperate-wizard'),
-    url(r'^abonnement/$', SubscriptionWizardView.as_view(), name='subscribe-wizard'),
+    #  url(r'^abonnement/$', SubscriptionWizardView.as_view(), name='subscribe-wizard'),
     url(r'^subscribers/$', subscribers_as_csv, name='subscribers-as-csv'),
     url(r'^cooperators/$', cooperators_as_csv, name='cooperators-as-csv'),
     url(r'^jadore/$', OrderWizardView.as_view(), name='order-wizard'),
