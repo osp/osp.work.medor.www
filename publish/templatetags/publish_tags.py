cd106427e26c1adf899004c53b4aa6013ad0c90a
Alex Leray <alexandre@stdin.fr>
2016-11-01 12:38:39 +0100

    typographic filters server-side

--- publish/templatetags/publish_tags.py
+++ publish/templatetags/publish_tags.py
@@ -1,5 +1,9 @@
 from django import template
 
+import html5lib
+from html5lib.filters import whitespace
+from html5lib_typogrify.french.filters import medor, figures
+
 
 register = template.Library()
 
@@ -7,3 +11,23 @@ register = template.Library()
 @register.filter(name='times')
 def times(number):
     return range(1, number + 1)
+
+
+@register.filter(name='typogrify')
+def typogrify(html):
+    # Using etree is important here because it does not suffer from a bug
+    # where a text featuring entitities is split into various
+    # adjacent text nodes.
+    # (thanks html5lib folks for the tip).
+    # See <https://github.com/html5lib/html5lib-python/issues/208>
+    dom = html5lib.parseFragment(html, treebuilder="etree")
+    walker = html5lib.getTreeWalker("etree")
+
+    stream = walker(dom)
+    stream = whitespace.Filter(stream)
+    stream = medor.Filter(stream)
+    stream = figures.Filter(stream)
+
+    s = html5lib.serializer.HTMLSerializer(quote_attr_values=True, omit_optional_tags=False)
+
+    return s.render(stream)
