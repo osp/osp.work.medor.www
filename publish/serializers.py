c5450ccf7bcf1a1ca971072b44f335305b46da43
Alex Leray <alexandre@stdin.fr>
2018-01-23 00:05:31 +0100

    Fix broken API

--- publish/serializers.py
+++ publish/serializers.py
@@ -3,6 +3,7 @@ from .models import Article, ArticleMembership, Issue, License, Rubric, Contribu
 
 
 class RubricSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:rubric-detail")
     id = serializers.ReadOnlyField()
 
     class Meta:
@@ -11,6 +12,7 @@ class RubricSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class LicenseSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:license-detail")
     id = serializers.ReadOnlyField()
 
     class Meta:
@@ -19,6 +21,7 @@ class LicenseSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:role-detail")
     id = serializers.ReadOnlyField()
 
     class Meta:
@@ -27,14 +30,16 @@ class RoleSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ContributorSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:contributor-detail")
     id = serializers.ReadOnlyField()
 
     class Meta:
         model = Contributor
-        fields = '__all__'
+        exclude = ('image',)
 
 
 class ContributionSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:contribution-detail")
     id = serializers.ReadOnlyField()
     contributor = ContributorSerializer()
     role = RoleSerializer()
@@ -42,13 +47,15 @@ class ContributionSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Contribution
-        fields = ['id', 'contributor', 'role', 'license']
+        fields = ['url', 'id', 'contributor', 'role', 'license']
 
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:article-detail")
     id = serializers.ReadOnlyField()
     rubric = RubricSerializer(required=False)
     contributions = ContributionSerializer(many=True)
+    license = LicenseSerializer()
 
     class Meta:
         model = Article
@@ -56,6 +63,7 @@ class ArticleSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ArticleMembershipSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:articlemembership-detail")
     id = serializers.ReadOnlyField()
     article = ArticleSerializer()
     folio = serializers.ReadOnlyField()
@@ -63,10 +71,11 @@ class ArticleMembershipSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ArticleMembership
         depth = 1
-        fields = '__all__'
+        exclude = ('issue',)
 
 
 class IssueSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:issue-detail")
     id = serializers.ReadOnlyField()
     articlemembership_set = ArticleMembershipSerializer(many=True)
 
