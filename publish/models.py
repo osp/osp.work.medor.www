be852b80444b548108b29b5d6acab47dac943439
Stéphanie Vilayphiou <stephanie@stdin.fr>
2018-02-06 11:20:22 +0100

    related_articles → optionnel

--- publish/models.py
+++ publish/models.py
@@ -138,7 +138,7 @@ class Article(models.Model):
     override_description = models.TextField('exergue spécifique pour le web', blank=True)
     override_image = FilerImageField(verbose_name='spécifier image aperçu', blank=True, null=True,
              on_delete=models.SET_NULL)
-    related_articles = models.ManyToManyField("self")
+    related_articles = models.ManyToManyField("self", blank=True)
 
 
     def save(self, *args, **kwargs):
